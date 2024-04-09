3c220fabb54064a398e2a6803ccc226ce81ff278
night <hitszzzf@gmail.com>
2024-04-09 13:22:38 -0400

    add nvboard

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,18 +1,21 @@
-#include <nvboard.h>
 #include <Vtop.h>
-
+#include <iostream>
+#include <nvboard.h>
 static TOP_NAME dut;
 
-void nvboard_bind_all_pins(TOP_NAME* top);
+void nvboard_bind_all_pins(TOP_NAME *top);
 
 static void single_cycle() {
-  dut.clk = 0; dut.eval();
-  dut.clk = 1; dut.eval();
+  dut.clk = 0;
+  dut.eval();
+  dut.clk = 1;
+  dut.eval();
 }
 
 static void reset(int n) {
   dut.rst = 1;
-  while (n -- > 0) single_cycle();
+  while (n-- > 0)
+    single_cycle();
   dut.rst = 0;
 }
 
@@ -21,9 +24,13 @@ int main() {
   nvboard_init();
 
   reset(10);
-
-  while(1) {
+  int i = 0;
+  int tem = 0;
+  while (1) {
+    i++;
     nvboard_update();
     single_cycle();
+    // if (i % 1000 == 0)
+    //   std::cin >> tem;
   }
 }

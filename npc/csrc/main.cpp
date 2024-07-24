523a34583a995cd7150152cd2bd1a65540aa2c54
night <hitszzzf@gmail.com>
2024-07-24 17:54:56 +0800

    init nvboard

---
+++ npc/csrc/main.cpp
@@ -0,0 +1,55 @@
+
+#include <Vtop.h>
+#include <nvboard.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+
+static Vtop dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+static void single_cycle()
+{
+    dut.clk = 0;
+    dut.eval();
+    dut.clk = 1;
+    dut.eval();
+}
+
+static void reset(int n)
+{
+    dut.rst = 1;
+    while (n-- > 0)
+        single_cycle();
+    dut.rst = 0;
+}
+
+int main(int argc, char** argv)
+{
+    Verilated::commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+
+    VerilatedVcdC* tfp = new VerilatedVcdC();
+    dut.trace(tfp, 0);
+    tfp->open("waveform.vcd");
+
+    // nvboard_bind_all_pins(&dut);
+    // nvboard_init();
+
+    reset(10);
+    int i = 0;
+    while (i++ <= 1000) {
+        // nvboard_update();
+        tfp->dump(i);
+        if (i % 2 == 0) {
+            dut.clk = 0;
+            dut.eval();
+        } else {
+            dut.clk = 1;
+            dut.eval();
+        }
+    }
+    dut.final();
+    tfp->close();
+    return 0;
+}
